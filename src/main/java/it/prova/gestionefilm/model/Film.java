d74308c372eeb37e376efac20ec2f7f3bb871dbd
fatma2705 <arafatma2003@gmail.com>
2024-05-10 09:42:04 +0200

    classe film modificata

--- src/main/java/it/prova/gestionefilm/model/Film.java
+++ src/main/java/it/prova/gestionefilm/model/Film.java
@@ -8,6 +8,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
@@ -35,6 +37,10 @@ public class Film {
 	private LocalDateTime createDateTime;
 	@UpdateTimestamp
 	private LocalDateTime updateDateTime;
+	
+	@ManyToOne
+    @JoinColumn(name="regista_id", nullable=false)
+    private Regista regista;
 
 	public Film() {
 
